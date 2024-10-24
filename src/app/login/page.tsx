81e55d7a267bd1c0974e90e3852b6e11de978b96
Agil Syofian Hidayat <agilsyofianh@gmail.com>
2024-10-24 14:13:33 +0700

    +- redirect login to indpis

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import {router} from "next/client";
+// import {router} from "next/client";
 
 import PageLoginView from "@/app/login/pageLoginView";
 import {useEffect} from "react";
@@ -10,7 +10,8 @@ const URL_SSO = process.env.NEXT_PUBLIC_SSO_URL_API;
 export default function PageLogin() {
   useEffect(() => {
     if (ENV !== 'dev') {
-      router.replace(URL_SSO ?? "https://indpis.bappenas.go.id")
+      // router.replace(URL_SSO ?? "https://indpis.bappenas.go.id")
+      window.location.replace(URL_SSO ?? "https://indpis.bappenas.go.id")
     }
   }, []);
   return <PageLoginView />
