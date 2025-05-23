84f34a28919bd1d42f24c41d9a46ca435e780419
caturteguhokgithub <caturteguhokgithub@gmail.com>
2025-05-23 10:05:57 +0700

    update loader style

--- src/app/components/loadingPage/index.tsx
+++ src/app/components/loadingPage/index.tsx
@@ -1,27 +1,36 @@
 import React from "react";
 import { Backdrop, CircularProgress } from "@mui/material";
 
-export default function LoadingPage({ gyro }: { gyro?: boolean }) {
- //  const [show, setShow] = React.useState(true);
+export default function LoadingPage({ name }: { name?: string }) {
+  //  const [show, setShow] = React.useState(true);
 
- //  React.useEffect(() => {
- //   let timer1 = setTimeout(() => setShow(false), 3 * 1000);
+  //  React.useEffect(() => {
+  //   let timer1 = setTimeout(() => setShow(false), 3 * 1000);
 
- //   return () => {
- //    clearTimeout(timer1);
- //   };
- //  });
+  //   return () => {
+  //    clearTimeout(timer1);
+  //   };
+  //  });
 
- return (
-  <Backdrop
-   sx={{
-    color: "#fff",
-    zIndex: (theme) => theme.zIndex.drawer + 1,
-    transition: "all 500ms ease",
-   }}
-   open
-  >
-   {gyro ? <div className="gyro"></div> : <CircularProgress color="inherit" />}
-  </Backdrop>
- );
+  return (
+    <Backdrop
+      sx={{
+        color: "#fff",
+        zIndex: (theme) => theme.zIndex.drawer + 1,
+        transition: "all 500ms ease",
+      }}
+      open
+    >
+      {name === "gyro" ? (
+        <div className="gyro"></div>
+      ) : name === "clones" ? (
+        <div className="clones-loader"></div>
+      ) : (
+        <>
+          <div className="clones-loader"></div>
+          {/* <CircularProgress color="inherit" /> */}
+        </>
+      )}
+    </Backdrop>
+  );
 }
--- src/app/log-activity/page.tsx
+++ src/app/log-activity/page.tsx
@@ -3,76 +3,77 @@
 import ContentPage from "@/app/components/contents";
 import React from "react";
 import DashboardLayout from "@/components/layouts/layout";
-import {MaterialReactTable, useMaterialReactTable} from "material-react-table";
-import {Box, Tooltip} from "@mui/material";
-import {usePermissionChecker} from "@/lib/core/helpers/authHelpers";
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
+import { Box, Tooltip } from "@mui/material";
+import { usePermissionChecker } from "@/lib/core/helpers/authHelpers";
 import useLogActivityVM from "@/app/log-activity/pageVM";
 import dayjs from "dayjs";
+import { blue, grey } from "@mui/material/colors";
+import { advancedTable } from "../components/table";
 
 export default function PageRoleManagement() {
+  usePermissionChecker("log-activity");
 
-  usePermissionChecker("log-activity")
-
-  const {
-    data
-  } = useLogActivityVM()
+  const { data } = useLogActivityVM();
 
   const columns = [
     {
       accessorKey: "created_at",
       header: "Timestamp",
       enableColumnFilterModes: true,
-      filterFns: 'contains',
-      Cell: ({renderedCellValue}: { renderedCellValue: any }) => (
-        dayjs(renderedCellValue).format('YYYY-MM-DD HH:mm:ss')
-      ),
+      filterFns: "contains",
+      Cell: ({ renderedCellValue }: { renderedCellValue: any }) =>
+        dayjs(renderedCellValue).format("YYYY-MM-DD HH:mm:ss"),
     },
     {
       accessorKey: "module",
       header: "Module",
       enableColumnFilterModes: true,
-      filterFns: 'contains'
+      filterFns: "contains",
     },
     {
       accessorKey: "activity",
       header: "Activity",
       enableColumnFilterModes: true,
-      filterFns: 'contains'
+      filterFns: "contains",
     },
     {
       accessorKey: "activity_group",
       header: "Action",
       enableColumnFilterModes: true,
-      filterFns: 'contains'
+      filterFns: "contains",
     },
     {
       accessorKey: "user_name",
       header: "User Name",
       enableColumnFilterModes: true,
-      filterFns: 'contains',
+      filterFns: "contains",
     },
     {
       accessorKey: "user_email",
       header: "User Email",
       enableColumnFilterModes: true,
-      filterFns: 'contains',
+      filterFns: "contains",
     },
     {
       accessorKey: "request_body",
       header: "Payload",
       enableColumnFilterModes: true,
-      filterFns: 'contains',
-      Cell: ({renderedCellValue}: { renderedCellValue: any }) => (
-        <Box sx={{
-          display: "block",
-          width: "200px",
-          whiteSpace: "nowrap",
-          overflow: "hidden",
-          textOverflow: "ellipsis",
-        }}>
-          <Tooltip title={renderedCellValue}>
-            {renderedCellValue}
-          </Tooltip>
+      filterFns: "contains",
+      Cell: ({ renderedCellValue }: { renderedCellValue: any }) => (
+        <Box
+          sx={{
+            display: "block",
+            width: "200px",
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
+          <Tooltip title={renderedCellValue}>{renderedCellValue}</Tooltip>
         </Box>
       ),
     },
@@ -81,13 +82,34 @@ export default function PageRoleManagement() {
   const table = useMaterialReactTable({
     columns,
     data,
-    initialState: {density: 'compact'},
+    // initialState: { density: "compact" },
     // enableRowNumbers: true,
+    ...advancedTable,
+    muiTableHeadCellProps: {
+      sx: {
+        bgcolor: blue[50],
+        justifyContent: "center",
+        borderLeft: `1px solid ${grey[300]}`,
+        borderBottom: "none",
+      },
+    },
+    muiTableBodyCellProps: {
+      sx: {
+        borderLeft: `1px solid ${grey[200]}`,
+        alignItems: "flex-start",
+        borderBottom: `1px solid ${grey[200]}`,
+      },
+    },
+    initialState: {
+      showGlobalFilter: true,
+    },
   });
 
-  return <DashboardLayout>
-    <ContentPage title="Log Activities">
-        <MaterialReactTable table={table}/>
-    </ContentPage>
-  </DashboardLayout>
+  return (
+    <DashboardLayout>
+      <ContentPage title="Log Activities" noMinusMargin>
+        <MaterialReactTable table={table} />
+      </ContentPage>
+    </DashboardLayout>
+  );
 }
