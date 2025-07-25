9adbf688dc488671b66c3bafc9a41beddc2b4a00
Ilham <ilhamuntung1902@gmail.com>
2025-07-25 10:45:52 +0700

    +- total

--- src/app/executive-summary/partials/tab6Critical/partials/tableCritical.tsx
+++ src/app/executive-summary/partials/tab6Critical/partials/tableCritical.tsx
@@ -345,29 +345,29 @@ export default function ProjectTable({
               </TableCell>
               {year === 0
                 ? [2025, 2026, 2027, 2028, 2029].map((year) => (
-                    <TableCell
-                      key={year}
-                      align="center"
-                      sx={{
-                        bgcolor: bgColorTh,
-                        textTransform: "uppercase",
-                      }}
-                    >
-                      {year}
-                    </TableCell>
-                  ))
+                  <TableCell
+                    key={year}
+                    align="center"
+                    sx={{
+                      bgcolor: bgColorTh,
+                      textTransform: "uppercase",
+                    }}
+                  >
+                    {year}
+                  </TableCell>
+                ))
                 : months.map((month) => (
-                    <TableCell
-                      key={month}
-                      align="center"
-                      sx={{
-                        bgcolor: bgColorTh,
-                        textTransform: "uppercase",
-                      }}
-                    >
-                      {month}
-                    </TableCell>
-                  ))}
+                  <TableCell
+                    key={month}
+                    align="center"
+                    sx={{
+                      bgcolor: bgColorTh,
+                      textTransform: "uppercase",
+                    }}
+                  >
+                    {month}
+                  </TableCell>
+                ))}
               {year > 0 && (
                 <TableCell
                   sx={{
@@ -499,12 +499,12 @@ export default function ProjectTable({
                             parent.kategori_proyek_id === 1
                               ? "#C63C51"
                               : parent.kategori_proyek_id === 2
-                              ? "#8C3061"
-                              : parent.kategori_proyek_id === 3
-                              ? "#FFD35A"
-                              : parent.kategori_proyek_id === 3
-                              ? "#FFA823"
-                              : "#DC0083",
+                                ? "#8C3061"
+                                : parent.kategori_proyek_id === 3
+                                  ? "#FFD35A"
+                                  : parent.kategori_proyek_id === 3
+                                    ? "#FFA823"
+                                    : "#DC0083",
                         }}
                       />
                     </Stack>
@@ -632,7 +632,7 @@ export default function ProjectTable({
                                 displayType="text"
                                 renderText={(value) => <b>{value}</b>}
                               />{" "}
-                              {formatNumberID(child.total_kegiatan)}{" "}
+                              {/* {formatNumberID(child.total_kegiatan)}{" "} */}
                               {child.satuan}
                             </TableCell>
                           )}
