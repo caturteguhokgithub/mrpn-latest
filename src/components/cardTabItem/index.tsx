3bf1c6ebd7a57c56df325cdb296e1c46475e6878
Ilham <ilhamuntung1902@gmail.com>
2025-08-08 09:20:26 +0700

    -- addButton

--- src/components/cardTabItem/index.tsx
+++ src/components/cardTabItem/index.tsx
@@ -270,7 +270,7 @@ export default function CardItem({
                       : null
                     : null}
                 {/* {addButton ? addButton : setting ? settingButton : null} */}
-                {addButton}
+                {/* {addButton} */}
                 {/* {setting && settingButton} */}
               </Stack>
             </>
