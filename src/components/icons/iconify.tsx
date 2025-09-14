c0f5356d3e327ca08df7a8879f986aff7fb612c0
caturteguhokgithub <caturteguhokgithub@gmail.com>
2025-09-15 05:54:12 +0700

    fix icon

--- src/components/icons/iconify.tsx
+++ src/components/icons/iconify.tsx
@@ -3,14 +3,24 @@ import { CSSProperties } from "react";
 
 export default function Iconify({
   name,
+  className,
   size,
   color,
   sx,
 }: {
   name: string;
+  className?: string;
   size?: number;
   color?: string;
   sx?: CSSProperties;
 }) {
-  return <Icon icon={name} height={size || 18} color={color} style={sx} />;
+  return (
+    <Icon
+      className={className}
+      icon={name}
+      height={size || 18}
+      color={color}
+      style={sx}
+    />
+  );
 }
